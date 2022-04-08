524edf26e3acd168f46f7e62c94ed18bf1b698fb
Washington-Fabiano <washington_vieira@outlook.com>
2022-04-08 19:05:45 -0300

    Tesla website

---
+++ scroll.js
@@ -0,0 +1,31 @@
+document.lastScrollPosition = 0;
+document.lastCentered = 0;
+document.onWayto = null;
+
+document.addEventListener('scroll', () => { 
+const direction = window.pageYOffset - document.lastScrollPosition > 0 ? 'down' : 'up';
+const sections = [...document.querySelectorAll('section')];
+
+if (document.onWayto === null) {
+    const destIndex = direction === 'up' ? document.lastCentered - 1 : document.lastCentered + 1;
+
+if (destIndex >= 0 && destIndex < sections.length) {
+    console.log({destIndex,direction});
+    document.onWayto = destIndex;
+    window.scroll(0, sections[destIndex].offsetTop);
+}
+}
+sections.forEach((section,index) => {
+    if (window.pageYOffset === section.offsetTop) { 
+        document.lastCentered = index;
+        section.className = 'active';
+        if (document.onWayto === index){
+            document.onWayto = null; 
+        }
+    } else {
+        section.className =''; 
+    }
+})
+
+    document.lastScrollPosition = window.pageYOffset;
+})
